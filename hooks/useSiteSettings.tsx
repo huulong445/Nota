08451b382dc643646d3e2e648c66bb79b636758d
huulong <huulongyenmy@gmail.com>
2026-01-03 22:41:05 +0700

    fix vercel build errors

--- hooks/useSiteSettings.tsx
+++ hooks/useSiteSettings.tsx
@@ -1,15 +1,16 @@
 import { create } from "zustand";
+import { Id } from "@/convex/_generated/dataModel";
 
 type SiteSettingsStore = {
   isOpen: boolean;
-  documentId?: string;
-  onOpen: (documentId: string) => void;
+  documentId?: Id<"documents">;
+  onOpen: (documentId: Id<"documents">) => void;
   onClose: () => void;
 };
 
 export const useSiteSettings = create<SiteSettingsStore>((set) => ({
   isOpen: false,
   documentId: undefined,
-  onOpen: (documentId: string) => set({ isOpen: true, documentId }),
+  onOpen: (documentId: Id<"documents">) => set({ isOpen: true, documentId }),
   onClose: () => set({ isOpen: false, documentId: undefined }),
 }));
